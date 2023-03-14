1091943c57bfe23c24fbb0ac96ae64029f2c2994
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-14 16:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your Script here.
+function rot13(str) {
+  let decodedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = ((charCode - 65 + 13) % 26) + 65;
+    }
+
+    decodedStr += String.fromCharCode(charCode);
+  }
+
+  return decodedStr;
+}
+
