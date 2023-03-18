e9b52e7a1206da9c8369e949cf60cae9b8f9d67c
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-18 14:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-// // Your Script here.
-// function rot13(str) {
-//   let decodedStr = "";
-
-//   for (let i = 0; i < str.length; i++) {
-//     let charCode = str.charCodeAt(i);
-
-//     if (charCode >= 65 && charCode <= 90) {
-//       charCode = ((charCode - 65 + 13) % 26) + 65;
-//     }
-
-//     decodedStr += String.fromCharCode(charCode);
-//   }
-
-//   return decodedStr;
-// }
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
+};
 
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
