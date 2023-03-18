8daf4f5fc050c7373f3f3b613174ff8f7f369562
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-18 14:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-// Your Script here.
-function rot13(str) {
-  let decodedStr = "";
+// // Your Script here.
+// function rot13(str) {
+//   let decodedStr = "";
 
-  for (let i = 0; i < str.length; i++) {
-    let charCode = str.charCodeAt(i);
+//   for (let i = 0; i < str.length; i++) {
+//     let charCode = str.charCodeAt(i);
 
-    if (charCode >= 65 && charCode <= 90) {
-      charCode = ((charCode - 65 + 13) % 26) + 65;
-    }
+//     if (charCode >= 65 && charCode <= 90) {
+//       charCode = ((charCode - 65 + 13) % 26) + 65;
+//     }
 
-    decodedStr += String.fromCharCode(charCode);
-  }
+//     decodedStr += String.fromCharCode(charCode);
+//   }
 
-  return decodedStr;
+//   return decodedStr;
+// }
+
+function rot13(encodedStr){
+   let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	const string = encodedStr;
+	const arr = [];
+ for(let i = 0;i<string.length;i++){
+	string.charAt(i) = string.charAt(i+13);
+	 arr.push(string[i]);
+ }
+  return ;//return decodedArr
 }
-
